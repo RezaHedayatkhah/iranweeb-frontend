9eed15a8ac1c68f3e7e2f5683bc2ab44028ffdc3
Reza Hedayatkhah <rezahedayat80@gmail.com>
2024-10-24 17:59:15 +0330

    fixed some bug z8

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -15,7 +15,7 @@ async function fetchPosts(params) {
             headers: {
                 Accept: "application/json",
             },
-            next: { tags: ['posts'], revalidate: 86400 },
+            next: {tags: ['posts'], revalidate: 86400},
         });
 
         if (!res.ok) throw new Error("Failed to fetch posts");
@@ -35,7 +35,7 @@ const fetchHeroPosts = async () => {
             headers: {
                 Accept: "application/json",
             },
-            next: { tags: ['posts'], revalidate: 86400 },
+            next: {tags: ['posts'], revalidate: 86400},
         });
         if (!res.ok) throw new Error("Failed to fetch hero posts");
 
@@ -47,46 +47,61 @@ const fetchHeroPosts = async () => {
 };
 
 export default async function Home() {
-    const latestPosts = await fetchPosts({ sortBy: "newest", pageSize: "10" });
-    const mostViewedPosts = await fetchPosts({ sortBy: "most_viewed", pageSize: "20" });
+    const latestPosts = await fetchPosts({sortBy: "newest", pageSize: "10"});
+    const mostViewedPosts = await fetchPosts({sortBy: "most_viewed", pageSize: "20"});
     const heroPosts = await fetchHeroPosts();
 
     return (
         <main>
             <div className="w-full h-screen">
-                <Hero posts={heroPosts} />
+                <Hero posts={heroPosts}/>
             </div>
-            <div className="flex flex-col items-center m-auto mt-10 gap-5 2xl:gap-10 text-black w-full px-5 md:px-0 md:w-9/12">
+            <div
+                className="flex flex-col items-center m-auto mt-10 gap-5 2xl:gap-10 text-black w-full px-5 md:px-0 md:w-9/12">
                 <div className="flex flex-col w-full items-center gap-y-5">
                     <div className="flex flex-col gap-5 w-full text-white py-3">
-                        <div className="flex justify-between w-full p-3 rounded-2xl bg-[#151f30]">
-                            <h2 className="border-b-2 border-red-500 w-fit text-lg">محبوب ترین ها</h2>
+                        <div
+                            className="flex md:flex-row flex-col items-center justify-between w-full py-5 px-8 rounded-2xl bg-[#151f30]">
+                            <div
+                                className={"flex items-center justify-between gap-1.5 py-1.5 bg-[#131720] rounded-2xl"}>
+                                <div
+                                    className={"h-8 px-2.5 rounded-xl flex justify-center items-center text-sm text-[#e0e0e0]"}>پیشنهادی
+                                </div>
+                                <div
+                                    className={"h-8 px-2.5 rounded-xl flex justify-center items-center text-sm text-[#e0e0e0]"}>محبوب
+                                    ‌ترین‌ها
+                                </div>
+                                <div
+                                    className={"h-8 px-2.5 rounded-xl flex justify-center items-center text-sm text-[#e0e0e0]"}>جدیدترین‌ها
+                                </div>
+                            </div>
+
                             <Link href="/search?sortBy=most_viewed"
-                                  className="flex items-center gap-2 cursor-pointer hover:text-blue-400">
-                                <p>نمایش همه</p>
-                                <FontAwesomeIcon icon={faArrowLeft} />
+                                  className="flex items-center gap-2 cursor-pointer text-[#e0e0e0] hover:text-red-500">
+                                <span className={"text-sm"}>جستجوی پیشرفته</span>
+                                <FontAwesomeIcon icon={faArrowLeft}/>
                             </Link>
                         </div>
                         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4  xl:grid-cols-5 gap-4 w-full">
                             {mostViewedPosts.length === 0 ? (
-                                Array(20).fill(null).map((_, idx) => <SkeletonPostCard key={idx} />)
+                                Array(20).fill(null).map((_, idx) => <SkeletonPostCard key={idx}/>)
                             ) : (
-                                mostViewedPosts.map((post) => <PostCard post={post} key={post.id} />)
+                                mostViewedPosts.map((post) => <PostCard post={post} key={post.id}/>)
                             )}
                         </div>
                     </div>
 
                     <div className="flex flex-col gap-5 w-full text-white py-3 overflow-x-hidden">
-                        <div className="flex justify-between w-full p-3 rounded-2xl bg-[#151f30]">
-                            <h2 className="border-b-2 border-red-500 w-fit text-lg">جدیدترین ها</h2>
-                            <Link href="/search?sortBy=newest"
-                                  className="flex items-center gap-2 cursor-pointer hover:text-blue-400">
-                                <p>نمایش همه</p>
-                                <FontAwesomeIcon icon={faArrowLeft} />
-                            </Link>
+                        <div className="w-full py-2">
+                            <h2 className=" w-fit text-4xl">جدیدترین ها</h2>
+                            {/*<Link href="/search?sortBy=newest"*/}
+                            {/*      className="flex items-center gap-2 cursor-pointer hover:text-blue-400">*/}
+                            {/*    <p>نمایش همه</p>*/}
+                            {/*    <FontAwesomeIcon icon={faArrowLeft}/>*/}
+                            {/*</Link>*/}
                         </div>
 
-                        <SwiperPosts posts={latestPosts} />
+                        <SwiperPosts posts={latestPosts}/>
                     </div>
                 </div>
             </div>
